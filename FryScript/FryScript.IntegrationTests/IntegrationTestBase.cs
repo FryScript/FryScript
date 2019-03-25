46062cc9359907b346ce4400e6215c2f3e6531c3
James Herring <james794251@hotmail.com>
2019-03-25 17:27:34 +0000

    Added integration tests project

---
+++ FryScript/FryScript.IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FryScript.IntegrationTests
+{
+    public abstract class IntegrationTestBase
+    {
+        public ScriptRuntime ScriptRuntime { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            ScriptRuntime = new ScriptRuntime();
+        }
+    }
+}
