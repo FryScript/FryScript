c82cd1a7b2e8a64a70fc0c3d3440827f39e49545
James Herring <james794251@hotmail.com>
2019-04-04 20:37:15 +0100

    Added integration tests

---
+++ FryScript/FryScript.IntegrationTests/Runtime/Eval/EvalTest.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FryScript.IntegrationTests.Runtime.Eval
+{
+    [TestClass]
+    public class EvalTests : IntegrationTestBase
+    {
+        [TestMethod]
+        public void Eval_Expression()
+        {
+            var result = ScriptRuntime.Eval("100;");
+
+            Assert.AreEqual(100, result);
+        }
+
+        [TestMethod]
+        public void Eval_Expression_Persists_Eval_State()
+        {
+            ScriptRuntime.Eval("var x = 100;");
+
+            var result = ScriptRuntime.Eval("x;");
+
+            Assert.AreEqual(100, result);
+        }
+    }
+}
---
+++ FryScript/FryScript.IntegrationTests/Runtime/New/NewTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FryScript.IntegrationTests.Runtime.New
+{
+    [TestClass]
+    public class NewTests : IntegrationTestBase
+    {
+        [TestMethod]
+        public void New_Script_From_Import()
+        {
+            dynamic script = ScriptRuntime.New("scripts/newTest");
+
+            Assert.AreEqual("test1", script.property1);
+            Assert.AreEqual(90, script.property2);
+        }
+    }
+}
