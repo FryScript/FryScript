8d0be73636a061ed44292e312710f8928c309244
James Herring <james794251@hotmail.com>
2019-05-21 22:08:43 +0100

    Additional script get tests

---
+++ FryScript/FryScript.IntegrationTests/Runtime/Get/GetThrowScriptTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FryScript.IntegrationTests.Runtime.Get
+{
+    [TestClass]
+    public class GetThrowScriptTests : IntegrationTestBase
+    {
+        [TestMethod]
+        [ExpectedException(typeof(FryScriptException))]
+        public void Get_Script_Throws_Exception()
+        {
+            ScriptRuntime.Get("scripts/throwScript");
+        }
+    }
+}
--- FryScript/FryScript/ScriptRuntime.cs
+++ FryScript/FryScript/ScriptRuntime.cs
@@ -50,7 +50,6 @@ namespace FryScript
             _objectFactory = objectFactory ?? throw new ArgumentNullException(nameof(objectFactory));
 
             Import(typeof(ScriptError));
-            Import(typeof(ScriptPrimitive<bool>));
         }
         public object Eval(string script)
         {
