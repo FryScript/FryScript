c60da48e8ee4bdd0a38aaef2384914f0367161da
James <james@james.com>
2019-07-10 16:34:29 +0100

    Improve yield return handling for fibre blocks by always appending a default yield return as the final statement

--- FryScript/FryScript/ScriptFibre.cs
+++ FryScript/FryScript/ScriptFibre.cs
@@ -43,167 +43,73 @@ namespace FryScript
     //}
 
 
-    //public abstract class PseudoFibre : ScriptFibre
-    //{
-    //    protected enum YieldType
-    //    { 
-    //        Yield,
-    //        YieldReturn
-    //    }
+    public abstract class PseudoFibre : ScriptFibre
+    {
+        protected enum YieldType
+        { 
+            Yield,
+            YieldReturn
+        }
 
-    //    protected struct YieldState
-    //    {
-    //        public YieldType YieldType;
+        protected struct YieldState
+        {
+            public YieldType YieldType;
 
-    //        public object Value;
+            public object Value;
 
-    //        public YieldState(YieldType yieldType, object value = null)
-    //        {
-    //            YieldType = yieldType;
-    //            Value = value;
-    //        }
-    //    }
+            public YieldState(YieldType yieldType, object value = null)
+            {
+                YieldType = yieldType;
+                Value = value;
+            }
+        }
 
-    //    public PseudoFibre()
-    //        : base(null)
-    //    {
-    //        Target = new Func<ScriptParams, ScriptFibreContext>(Invoke);
-    //    }
+        protected PseudoFibre()
+            : base(new Func<ScriptParams, ScriptFibreContext>(s => null))
+        {
+            TargetDelegate = new Func<ScriptParams, ScriptFibreContext>(Invoke);
+        }
 
-    //    private ScriptFibreContext Invoke(ScriptParams args)
-    //    {
-    //        var state = Fibre(args).GetEnumerator();
+        private ScriptFibreContext Invoke(ScriptParams args)
+        {
+            var state = Fibre(args).GetEnumerator();
 
-    //        var context = new ScriptFibreContext(c =>
-    //        {
-    //            c.YieldState = 0;
+            var context = new ScriptFibreContext(c =>
+            {
+                c.YieldState = 0;
 
-    //            if (!state.MoveNext())
-    //                ExceptionHelper.FibreContextCompleted();
+                if (!state.MoveNext())
+                    ExceptionHelper.FibreContextCompleted();
 
-    //            if (state.Current.YieldType == YieldType.YieldReturn)
-    //                c.YieldState = ScriptFibreContext.CompletedState;
+                if (state.Current.YieldType == YieldType.YieldReturn)
+                    c.YieldState = ScriptFibreContext.CompletedState;
 
-    //            return state.Current.Value;
-    //        });
+                return state.Current.Value;
+            });
 
-    //        return context;
-    //    }
+            return context;
+        }
 
-    //    protected YieldState Pause()
-    //    {
-    //        return new YieldState(YieldType.Yield, ScriptFibreContext.NoResult);
-    //    }
+        protected YieldState Pause()
+        {
+            return new YieldState(YieldType.Yield, ScriptFibreContext.NoResult);
+        }
 
-    //    protected YieldState Pause(object value)
-    //    {
-    //        return new YieldState(YieldType.Yield, value);
-    //    }
+        protected YieldState Pause(object value)
+        {
+            return new YieldState(YieldType.Yield, value);
+        }
 
-    //    protected YieldState Complete()
-    //    {
-    //        return new YieldState(YieldType.YieldReturn, ScriptFibreContext.NoResult);
-    //    }
+        protected YieldState Complete()
+        {
+            return new YieldState(YieldType.YieldReturn, ScriptFibreContext.NoResult);
+        }
 
-    //    protected YieldState Complete(object value)
-    //    {
-    //        return new YieldState(YieldType.YieldReturn, value);
-    //    }
+        protected YieldState Complete(object value)
+        {
+            return new YieldState(YieldType.YieldReturn, value);
+        }
 
-    //    protected abstract IEnumerable<YieldState> Fibre(ScriptParams args);
-    //}
-
-    //public abstract class NativeFibre : ScriptFibre
-    //{
-    //    protected enum YieldState
-    //    {
-    //        Yield,
-    //        YieldReturn
-    //    }
-
-    //    protected struct Yield
-    //    {
-    //        public readonly YieldState YieldState;
-    //        public readonly object Value;
-
-    //        internal Yield(YieldState yieldState, object value = null)
-    //        {
-    //            YieldState = yieldState;
-    //            Value = value ?? ScriptFibreContext.NoResult;
-    //        }
-    //    }
-
-    //    public NativeFibre() : base(null)
-    //    {
-    //        Target = new Func<ScriptParams, ScriptFibreFrame>(Invoke);
-    //    }
-
-    //    protected Yield Empty()
-    //    {
-    //        return new Yield(YieldState.Yield);
-    //    }
-
-    //    protected Yield EmptyReturn()
-    //    {
-    //        return new Yield(YieldState.YieldReturn);
-    //    }
-
-    //    protected Yield Value(object value)
-    //    {
-    //        return new Yield(YieldState.Yield, value);
-    //    }
-
-    //    protected Yield ValueReturn(object value)
-    //    {
-    //        return new Yield(YieldState.YieldReturn, value);
-    //    }
-
-    //    private ScriptFibreFrame Invoke(ScriptParams args)
-    //    {
-    //        IEnumerator<Yield> enumerator = null;
-            
-    //        var frame = new ScriptFibreFrame();
-    //        frame.Init(c =>
-    //        {
-    //            enumerator = enumerator ?? Fibre(c, args);
-
-    //            frame.YieldState = 0;
-
-    //            if (!enumerator.MoveNext())
-    //            {
-    //                frame.YieldState = ScriptFibreContext.CompletedState;
-    //                return enumerator.Current;
-    //            }
-
-    //            var yield = enumerator.Current;
-
-    //            if (yield.YieldState == YieldState.YieldReturn)
-    //                frame.YieldState = ScriptFibreContext.CompletedState;
-
-    //            return yield.Value;
-    //        });
-
-    //        return frame;
-    //    }
-
-    //    protected abstract IEnumerator<Yield> Fibre(ScriptFibreContext context, ScriptParams args);
-    //}
-
-    //public class AfterFibre : NativeFibre
-    //{
-    //    protected override IEnumerator<Yield> Fibre(ScriptFibreContext context, ScriptParams args)
-    //    {
-    //        var length = args.Get<int>(0);
-    //        var func = args.Get<ScriptFunction>(1);
-
-    //        var endTime = DateTime.UtcNow.AddSeconds(length);
-
-    //        while(DateTime.UtcNow < endTime)
-    //        {
-    //            yield return Empty(); 
-    //        }
-
-    //        yield return ValueReturn(func.Invoke<object>());
-    //    }
-    //}
+        protected abstract IEnumerable<YieldState> Fibre(ScriptParams args);
+    }
 }
