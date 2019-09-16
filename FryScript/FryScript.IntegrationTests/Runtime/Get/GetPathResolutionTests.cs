0cef8e425932f13daf4aa153c2242e80a7d5898d
James Herring <james794251@hotmail.com>
2019-09-16 20:27:11 +0100

    Added path resolution integration tests

---
+++ FryScript/FryScript.IntegrationTests/Runtime/Get/GetPathResolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FryScript.IntegrationTests.Runtime.Get
+{
+    [TestClass]
+    public class GetPathResolutionTests : IntegrationTestBase
+    {
+        [TestMethod]
+        public void Get_Resolves_Script_With_Same_Name_But_Different_Paths_From_Multiple_Locations()
+        {
+            dynamic obj = ScriptRuntime.Get("Scripts/ResolutionScripts/SubFolder/inheriting");
+
+            Assert.AreNotEqual(obj.toImport, obj.toImport2);
+        }
+
+        [TestMethod]
+        public void Get_Resolves_Script_From_Higher_Directory()
+        {
+            dynamic toResolve = ScriptRuntime.Get("Scripts/ResolutionScripts/toResolve");
+            dynamic resolveHigher = ScriptRuntime.Get("Scripts/ResolutionScripts/SubFolder/resolveHigher");
+
+            Assert.AreEqual(toResolve, resolveHigher.toResolve);
+        }
+    }
+}
