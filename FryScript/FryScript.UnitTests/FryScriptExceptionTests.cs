1c2e67281b342e4c15b89bcc770ec28dc8fc9bf5
James Herring <james@james.com>
2020-10-13 19:40:27 +0100

    More unit tests

--- FryScript/FryScript.UnitTests/FryScriptExceptionTests.cs
+++ FryScript/FryScript.UnitTests/FryScriptExceptionTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FryScript.UnitTests
@@ -6,9 +7,43 @@ namespace FryScript.UnitTests
     public class FryScriptExceptionTests
     {
         [TestMethod]
-        public void Write_Some_Tests()
+        public void Ctor_Sets_Message()
         {
-            Assert.Fail();
+            var ex = new FryScriptException("Test");
+
+            Assert.AreEqual("Test", ex.Message);
+        }
+
+        [TestMethod]
+        public void Ctor_Sets_Message_Inner_Exception_Name_Line_And_Column()
+        {
+            var innerEx = new Exception();
+            var ex = new FryScriptException("Test", innerEx, "Name", 1, 2);
+
+            Assert.AreEqual("Test", ex.Message);
+            Assert.AreEqual(innerEx, ex.InnerException);
+            Assert.AreEqual("Name", ex.Name);
+            Assert.AreEqual(1, ex.Line);
+            Assert.AreEqual(2, ex.Column);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FormatException_Null_Ex()
+        {
+            FryScriptException.FormatException(null, "name", 1, 2);
+        }
+
+        [TestMethod]
+        public void FormatException_Wraps_Non_FryScriptException()
+        {
+            var expectedEx = new Exception();
+            var ex = FryScriptException.FormatException(expectedEx, "Name", 1, 2) as FryScriptException;
+
+            Assert.AreEqual(expectedEx, ex.InnerException);
+            Assert.AreEqual("Name", ex.Name);
+            Assert.AreEqual(1, ex.Line);
+            Assert.AreEqual(2, ex.Column);
         }
     }
 }
