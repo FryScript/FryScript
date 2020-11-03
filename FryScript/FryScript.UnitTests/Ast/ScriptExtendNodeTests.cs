b266bde9d94b970f3c9e0d23d1816b8865767a04
James Herring <james794251@hotmail.com>
2020-11-03 14:20:32 +0000

    Script extend node tests

--- FryScript/FryScript.UnitTests/Ast/ScriptExtendNodeTests.cs
+++ FryScript/FryScript.UnitTests/Ast/ScriptExtendNodeTests.cs
@@ -1,7 +1,11 @@
 ﻿using FryScript.Ast;
 using FryScript.Compilation;
+using FryScript.Parsing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
 using System;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace FryScript.UnitTests.Ast
 {
@@ -26,9 +30,39 @@ namespace FryScript.UnitTests.Ast
         }
 
         [TestMethod]
-        public void MyTestMethod()
+        public void GetExpression_Extends_Script()
         {
-            Assert.Fail();
+            Node.StubCompilerContext();
+
+            var nameNode = Node<AstNode>.WithValueString("script");
+
+            Node.SetChildren(null, nameNode);
+
+            var extendScript = new ScriptObject();
+            Node.CompilerContext.ScriptRuntime.Get(
+                "script",
+                Node.CompilerContext.Uri)
+                .Returns(extendScript);
+
+            Scope.AddMember(Keywords.This, Node, typeof(IScriptObject));
+
+            var result = Node.GetExpression(Scope) as MethodCallExpression;
+
+            Assert.AreEqual(1, result.Arguments.Count);
+            Assert.AreEqual(Scope.GetMemberExpression(Keywords.This), result.Arguments.Single());
+
+            Assert.AreEqual(extendScript.GetType(), Node.CompilerContext.ScriptType);
+            Assert.AreEqual(extendScript.ObjectCore.Builder, Node.CompilerContext.ScriptObjectBuilder);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CompilerException))]
+        public void GetExpression_Throws_Compiler_Exception_In_Eval_Mode()
+        {
+            Node.StubParseNode();
+            Node.StubCompilerContext(isEvalMode: true);
+
+            Node.GetExpression(Scope);
         }
     }
 }
---
+++ FryScript/FryScript.UnitTests/Ast/ScriptHeadersNodeTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FryScript.UnitTests.Ast
+{
+    class ScriptHeadersNodeTests
+    {
+    }
+}
